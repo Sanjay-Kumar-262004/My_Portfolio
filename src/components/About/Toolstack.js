e6b9f49282373c130ea1fee8fb68a4189adfb03c
Sanjay-Kumar-262004 <sanjaykumar.ece26@gmail.com>
2024-06-07 23:23:22 +0530

    Vercel Added

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
 import { FaGithub } from "react-icons/fa";
+import { TbBrandVercel } from "react-icons/tb";
 import {
   SiVisualstudiocode,
   // SiPostman,
@@ -22,6 +23,10 @@ function Toolstack() {
       <Col xs={4} md={2} className="tech-icons">
       <FaGithub />
         <h5>GitHub</h5>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+      <TbBrandVercel />
+        <h5>Vercel</h5>
       </Col>
       {/* <Col xs={4} md={2} className="tech-icons">
         <SiPostman />
