fcaca73f87525931b0ff9e6860fbe4d1d261d730
Sanjay-Kumar-262004 <sanjaykumar.ece26@gmail.com>
2024-08-23 14:13:11 +0530

    Slider image position updated

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -96,52 +96,57 @@ function Projects() {
           </Col>
 
           <Col md={4} className="project-card">
-            <ProjectCard
-              isProject={false}
-              title={<strong className="purple">ECOBIN HACKATHON</strong>}
-              slides={
-                <Slide autoplay={false}>
-                  <div className="each-slide-effect">
-                    <div
-                      style={{
-                        backgroundImage: `url(${ecobin})`,
-                        backgroundPosition: 'center',
-                        height: '300px'
-                      }}
-                    >
-                    </div>
-                  </div>
-                  <div className="each-slide-effect">
-                    <div
-                      style={{
-                        backgroundImage: `url(${ecobin2})`,
-                        backgroundPosition: 'center',
-                        height: '300px'
-                      }}
-                    >
-                    </div>
-                  </div>
-                </Slide>
-              }
-              description={(
-                <span>
-                  Participated in the 'Smart ECOBIN Hackathon' along with my teammates&nbsp;
-                  <strong className="purple">
-                    <a href="https://www.linkedin.com/in/rohanrdy/" target="_blank" rel="noreferrer" style={{ textDecoration: "none", color: "inherit" }}>
-                      Rohan Reddy
-                    </a>
-                  </strong>&nbsp;and&nbsp;
-                  <strong className="purple">
-                    <a href="https://www.linkedin.com/in/shivaram-kumar-v-r-b75919257/" target="_blank" rel="noreferrer" style={{ textDecoration: "none", color: "inherit" }}>
-                      Shivaram Kumar V.R
-                    </a>
-                  </strong>, organized on 
-                  <strong className="purple">'Engineers Day - 23'</strong> by the Department of Mechanical Engineering at SRM Institute of Science and Technology (SRMIST). It was an incredible experience collaborating with talented individuals to innovate and develop sustainable solutions for a cleaner and greener future. During the Smart ECOBIN Hackathon, I had the privilege of witnessing some 
-                  <strong className="purple">&nbsp;truly innovative ideas</strong> from fellow teams.
-                </span>
-              )}
-            />
-          </Col>
+  <ProjectCard
+    isProject={false}
+    slides={
+      <Slide autoplay={false}>
+        <div className="each-slide-effect">
+          <div
+            style={{
+              backgroundImage: `url(${ecobin})`,
+              backgroundPosition: 'center',
+              height: '300px',
+            }}
+          >
+          </div>
+        </div>
+        <div className="each-slide-effect">
+          <div
+            style={{
+              backgroundImage: `url(${ecobin2})`,
+              backgroundPosition: 'center',
+              height: '300px',
+            }}
+          >
+          </div>
+        </div>
+      </Slide>
+    }
+    description={(
+      <div>
+        <h3 style={{ color: 'white', marginTop: '15px' }}>
+          <strong className="purple">ECOBIN HACKATHON</strong>
+        </h3>
+        <span style={{ color: 'white' }}>
+          Participated in the 'Smart ECOBIN Hackathon' along with my teammates&nbsp;
+          <strong className="purple">
+            <a href="https://www.linkedin.com/in/rohanrdy/" target="_blank" rel="noreferrer" style={{ textDecoration: "none", color: "inherit" }}>
+              Rohan Reddy
+            </a>
+          </strong>&nbsp;and&nbsp;
+          <strong className="purple">
+            <a href="https://www.linkedin.com/in/shivaram-kumar-v-r-b75919257/" target="_blank" rel="noreferrer" style={{ textDecoration: "none", color: "inherit" }}>
+              Shivaram Kumar V.R
+            </a>
+          </strong>, organized on 
+          <strong className="purple">'Engineers Day - 23'</strong> by the Department of Mechanical Engineering at SRM Institute of Science and Technology (SRMIST). It was an incredible experience collaborating with talented individuals to innovate and develop sustainable solutions for a cleaner and greener future. During the Smart ECOBIN Hackathon, I had the privilege of witnessing some 
+          <strong className="purple">&nbsp;truly innovative ideas</strong> from fellow teams.
+        </span>
+      </div>
+    )}
+  />
+</Col>
+
         </Row>
 
         <h1 className="project-heading" style={{ marginTop: "50px" }}>
@@ -151,41 +156,47 @@ function Projects() {
           Here are some volunteering activities I've been involved in
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-          <Col md={4} className="project-card">
-            <ProjectCard
-              isProject={false}
-              title={<strong className="purple">Emerging Trends in IoT and It's Applications</strong>}
-              slides={
-                <Slide autoplay={false}>
-                  <div className="each-slide-effect">
-                    <div
-                      style={{
-                        backgroundImage: `url(${volunteer1})`,
-                        backgroundPosition: 'center',
-                        height: '300px'
-                      }}
-                    >
-                    </div>
-                  </div>
-                  <div className="each-slide-effect">
-                    <div
-                      style={{
-                        backgroundImage: `url(${volunteer1_2})`,
-                        backgroundPosition: 'center',
-                        height: '300px'
-                      }}
-                    >
-                    </div>
-                  </div>
-                </Slide>
-              }
-              description={(
-                <span>
-                  I Was one of the <strong className="purple">Student Coordinators</strong> for engaging an informative workshop: 'Emerging Trends in IoT and its Applications', a <strong className="purple">6-day workshop</strong> organized by the ECE SRMIST KTR Department as part of SRMIST KTR'S Professional Development Programme.
-                </span>
-              )}
-            />
-          </Col>
+        <Col md={4} className="project-card">
+  <ProjectCard
+    isProject={false}
+    slides={
+      <div>
+        <Slide autoplay={false}>
+          <div className="each-slide-effect">
+            <div
+              style={{
+                backgroundImage: `url(${volunteer1})`,
+                backgroundPosition: 'center',
+                height: '300px',
+              }}
+            >
+            </div>
+          </div>
+          <div className="each-slide-effect">
+            <div
+              style={{
+                backgroundImage: `url(${volunteer1_2})`,
+                backgroundPosition: 'center',
+                height: '300px',
+              }}
+            >
+            </div>
+          </div>
+        </Slide>
+        <h4 style={{ textAlign: 'center', color: 'white' }}>
+          <strong className="purple">Emerging Trends in IoT and It's Applications</strong>
+        </h4>
+        <div style={{ marginTop: '10px', color: 'white' }}>
+          <span>
+            I was one of the <strong className="purple">Student Coordinators</strong> for engaging an informative workshop: 'Emerging Trends in IoT and its Applications', a <strong className="purple">6-day workshop</strong> organized by the ECE SRMIST KTR Department as part of SRMIST KTR's Professional Development Programme.
+          </span>
+        </div>
+      </div>
+    }
+  />
+</Col>
+
+
 
           <Col md={4} className="project-card">
             <ProjectCard
