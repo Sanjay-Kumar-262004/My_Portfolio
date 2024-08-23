21e44bb7b961063b940d06a39ea15a568f55c25f
Sanjay-Kumar-262004 <sanjaykumar.ece26@gmail.com>
2024-08-23 14:30:02 +0530

    Type Writter updated

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -1,44 +1,34 @@
 import React from "react";
 import Typewriter from "typewriter-effect";
 
+
 function Type() {
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        textAlign: "center",
-        width: "100%",
-        padding: "20px",
+    <Typewriter
+      options={{
+        autoStart: true,
+        loop: true,
+        deleteSpeed: 50,
       }}
-    >
-      <Typewriter
-        options={{
-          autoStart: true,
-          loop: true,
-          deleteSpeed: 50,
-        }}
-        onInit={(typewriter) => {
-          typewriter
-            .typeString('<span style="color: white; font-size: 2rem;">I DO </span><span style="color: white; font-size: 2rem;">Code</span>')
-            .pauseFor(1000)
-            .deleteChars(4)
-            .typeString('<span style="color: white; font-size: 2rem;">Read Books</span>')
-            .pauseFor(1000)
-            .deleteChars(10)
-            .typeString('<span style="color: white; font-size: 2rem;">Develop Sites</span>')
-            .pauseFor(1000)
-            .deleteChars(13)
-            .typeString('<span style="color: white; font-size: 2rem;">Follow Tech</span>')
-            .pauseFor(1000)
-            .deleteChars(11)
-            .typeString('<span style="color: white; font-size: 2rem;">Represent My Class</span>')
-            .pauseFor(1000)
-            .start();
-        }}
-      />
-    </div>
+      onInit={(typewriter) => {
+        typewriter
+          .typeString('<span style="color: white;">I DO </span>Code')
+          .pauseFor(1000)
+          .deleteChars(4)
+          .typeString('Read Books')
+          .pauseFor(1000)
+          .deleteChars(10)
+          .typeString('Develop Sites')
+          .pauseFor(1000)
+          .deleteChars(13)
+          .typeString('Follow Tech')
+          .pauseFor(1000)
+          .deleteChars(11)
+          .typeString('Represent My Class')
+          .pauseFor(1000)
+          .start();
+      }}
+    />
   );
 }
 
