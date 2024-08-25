1cba1326c14551f223dd644daccddd4a6af9d8f1
Sanjay-Kumar-262004 <sanjaykumar.ece26@gmail.com>
2024-08-25 14:06:18 +0530

    Table Styling updated

--- src/components/About/AcademicPerformance.js
+++ src/components/About/AcademicPerformance.js
@@ -2,85 +2,104 @@ import React from "react";
 import { Table } from "react-bootstrap";
 
 function AcademicPerformance() {
+  const tableStyle = {
+    backgroundColor: 'transparent',
+    border: '2.2px solid #F7EEDD',
+  };
+
+  const thStyle = {
+    backgroundColor: 'transparent',
+    color: '#008DDA',
+    fontWeight: 'Bold'  
+  };
+
+  const tdStyle = {
+    backgroundColor: 'transparent',
+    color: '#EEF7FF',
+    
+  };
+
   return (
     <div className="academic-performance">
       {/* Table for X and XII */}
-      <Table striped bordered hover responsive="sm" style={{ backgroundColor: 'transparent' }}>
-        <thead style={{ backgroundColor: 'transparent' }}>
+      <Table striped bordered hover responsive="sm" style={tableStyle}>
+        <thead style={tableStyle}>
           <tr>
-            <th>Education Level</th>
-            <th>Marks</th>
-            <th>Percentage</th>
+            <th style={thStyle}>Education Level</th>
+            <th style={thStyle}>Marks</th>
+            <th style={thStyle}>Percentage</th>
           </tr>
         </thead>
-        <tbody style={{ backgroundColor: 'transparent' }}>
+        <tbody style={tableStyle}>
           <tr>
-            <td>Xth</td>
-            <td>456/500</td>
-            <td>91.2%</td>
+            <td style={tdStyle}>Xth Standard</td>
+            <td style={tdStyle}>456/500</td>
+            <td style={tdStyle}>91.2%</td>
           </tr>
           <tr>
-            <td>XIIth</td>
-            <td>555.56/600</td>
-            <td>92.59%</td>
+            <td style={tdStyle}>XIIth Standard</td>
+            <td style={tdStyle}>555.56/600</td>
+            <td style={tdStyle}>92.59%</td>
           </tr>
         </tbody>
       </Table>
-
+      <h4 style={{ color: "white" }}>
+          College Academics
+        </h4>
       {/* Table for College Semesters */}
-      <Table striped bordered hover responsive="sm" style={{ backgroundColor: 'transparent' }}>
-        <thead style={{ backgroundColor: 'transparent' }}>
+      <Table striped bordered hover responsive="sm" style={tableStyle}>
+        <thead style={tableStyle}>
           <tr>
-            <th>Semester</th>
-            <th>CGPA</th>
-            <th>Percentage</th>
+            <th style={thStyle}>Semester</th>
+            <th style={thStyle}>CGPA/10</th>
+            <th style={thStyle}>Percentage/100</th>
           </tr>
         </thead>
-        <tbody style={{ backgroundColor: 'transparent' }}>
+        <tbody style={tableStyle}>
           <tr>
-            <td>Semester 1</td>
-            <td>9.8</td>
-            <td>98%</td>
+            <td style={tdStyle}>Semester 1</td>
+            <td style={tdStyle}>9.8</td>
+            <td style={tdStyle}>98%</td>
           </tr>
           <tr>
-            <td>Semester 2</td>
-            <td>8.67</td>
-            <td>86.7%</td>
+            <td style={tdStyle}>Semester 2</td>
+            <td style={tdStyle}>8.67</td>
+            <td style={tdStyle}>86.7%</td>
           </tr>
           <tr>
-            <td>Semester 3</td>
-            <td>7.33</td>
-            <td>73.3%</td>
+            <td style={tdStyle}>Semester 3</td>
+            <td style={tdStyle}>7.33</td>
+            <td style={tdStyle}>73.3%</td>
           </tr>
           <tr>
-            <td>Semester 4</td>
-            <td>8.18</td>
-            <td>81.8%</td>
+            <td style={tdStyle}>Semester 4</td>
+            <td style={tdStyle}>8.18</td>
+            <td style={tdStyle}>81.8%</td>
           </tr>
           <tr>
-            <td>Semester 5</td>
-            <td>8.52</td>
-            <td>85.2%</td>
+            <td style={tdStyle}>Semester 5</td>
+            <td style={tdStyle}>8.52</td>
+            <td style={tdStyle}>85.2%</td>
           </tr>
           <tr>
-            <td>Semester 6</td>
-            <td>9.12</td>
-            <td>91.2%</td>
+            <td style={tdStyle}>Semester 6</td>
+            <td style={tdStyle}>9.12</td>
+            <td style={tdStyle}>91.2%</td>
           </tr>
           <tr>
-            <td>Semester 7</td>
-            <td></td>
-            <td></td>
+            <td style={tdStyle}>Semester 7</td>
+            <td style={tdStyle}>TBA</td>
+            <td style={tdStyle}>TBA</td>
           </tr>
           <tr>
-            <td>Semester 8</td>
-            <td></td>
-            <td></td>
+            <td style={tdStyle}>Semester 8</td>
+            <td style={tdStyle}>TBA</td>
+            <td style={tdStyle}>TBA</td>
           </tr>
           <tr>
-            <td>Overall CGPA & Percentage</td>
-            <td>8.58</td>
-            <td>85.8%</td>
+            <td style={thStyle}>Overall CGPA & Percentage </td>
+            <td style={thStyle}>8.58</td>
+            <td style={thStyle}>85.8%</td>
           </tr>
         </tbody>
       </Table>
