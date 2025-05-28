435605e40a906427b9d333d1beead6792e6da7b9
Sanjay-Kumar-262004 <sanjaykumar.ece26@gmail.com>
2025-05-28 16:24:54 +0530

    About Updated

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -9,7 +9,7 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Greetings, I am <span className="purple">Sanjay Kumar S </span>, a dedicated <span className="purple">Electronics and Communication Engineering</span> student at <span className="purple">SRM University's KTR campus </span>
+            Greetings, I am <span className="purple">Sanjay Kumar S </span>, a dedicated <span className="purple">Electronics and Communication Engineering</span> graduate from <span className="purple">SRM University's KTR campus </span>
             from <span className="purple"> Tamilnadu, India </span>.
             Throughout my academic journey, I have cultivated a passion for Technology-Driven Problem-Solving and Innovative solutions. As an aspiring developer, I am steadfast in honing my skills in <span className="purple">Programming and Web Development</span> . I received <span className="purple">The Best Student Award</span> from <span className="purple">The Institution of Engineers (India)</span> which reflects my dedication to both academic and extracurricular activities.
             My unique background in Electronics and Communication Engineering, coupled with my burgeoning expertise in Web Development, positions me as a <span className="purple">quick learner and a committed team player</span>. I actively pursue new challenges that not only foster my personal growth but also allow me to expand my skill set. I am confident that my technical acumen, paired with adept problem-solving abilities, renders me a valuable asset to any team.
