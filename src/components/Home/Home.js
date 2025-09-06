a7a3257c2ed085186f84b60e1427bc6151c12bdb
Sanjay-Kumar-262004 <sanjaykumar.ece26@gmail.com>
2025-09-06 22:10:33 +0530

    svg updated

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import homeLogo from "../../Assets/home-main.svg";
+import homeLogo2 from "../../Assets/home-main1.svg";
 import Particle from "../Particle";
 // import Home2 from "./Home2";
 import Type from "./Type";
@@ -31,7 +32,7 @@ function Home() {
 
             <Col md={5} style={{ paddingBottom: 20 }}>
               <img
-                src={homeLogo}
+                src={homeLogo2}
                 alt="home pic"
                 className="img-fluid"
                 style={{ maxHeight: "450px" }}
