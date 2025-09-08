1652bc3f9ff2f047b5ea8604da533d70c7ce4145
Sanjay-Kumar-262004 <sanjaykumar.ece26@gmail.com>
2025-09-08 09:57:26 +0530

    Conflicts pn these files index and footer commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -28,16 +28,7 @@ function Footer() {
                 <AiFillGithub />
               </a>
             </li>
-            {/* <li className="social-icons">
-              <a
-                href="https://twitter.com/Soumyajit4419"
-                style={{ color: "white" }}
-                target="_blank" 
-                rel="noopener noreferrer"
-              >
-                <AiOutlineTwitter />
-              </a>
-            </li> */}
+    
             <li className="social-icons">
               <a
                 href="https://www.linkedin.com/in/sanjay-kumar-s26/"
@@ -48,16 +39,7 @@ function Footer() {
                 <FaLinkedinIn />
               </a>
             </li>
-            {/* <li className="social-icons">
-              <a
-                href="https://www.instagram.com/soumyajit4419"
-                style={{ color: "white" }}
-                target="_blank" 
-                rel="noopener noreferrer"
-              >
-                <AiFillInstagram />
-              </a>
-            </li> */}
+
           </ul>
         </Col>
       </Row>
