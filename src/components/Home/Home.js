710d68800e828439e1f6b9243456dbe129bb84d2
Sanjay-Kumar-262004 <sanjaykumar.ece26@gmail.com>
2025-09-08 11:12:53 +0530

    Background and Bot image updated

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import homeLogo1 from "../../Assets/home-main1.svg";
+import homeLogo2 from "../../Assets/bot.png";
 import Particle from "../Particle";
 import Type from "./Type";
 
@@ -29,7 +30,7 @@ function Home() {
             </Col>
             <Col md={5} style={{ paddingBottom: 20, textAlign: "center" }}>
               <img
-                src={homeLogo1}
+                src={homeLogo2}
                 alt="home pic"
                 className="img-fluid floating-img"
               />
