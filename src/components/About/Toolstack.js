8b6a6f97dea6dd116b4ac0353d26a5362015a1a0
Sanjay-Kumar-262004 <sanjaykumar.ece26@gmail.com>
2026-04-25 12:05:37 +0530

    Refactored portfolio for Rockwell Automation

---
+++ src/components/About/Toolstack.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import { FaCogs, FaProjectDiagram, FaIndustry } from "react-icons/fa";
+import { MdOutlinePrecisionManufacturing } from "react-icons/md";
+
+function Toolstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <FaIndustry />
+        <h5>Studio 5000</h5>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <FaIndustry />
+        <h5>FactoryTalk View</h5>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <FaIndustry />
+        <h5>RSLogix 500</h5>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <FaIndustry />
+        <h5>RSLinx Classic</h5>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <MdOutlinePrecisionManufacturing />
+        <h5>Connected Components Workbench</h5>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons" style={{ opacity: 0.5 }}>
+        <FaCogs />
+        <h5>Future Tool</h5>
+      </Col>
+    </Row>
+  );
+}
+
+export default Toolstack;
+
